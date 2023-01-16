03fbd813994bc014d842f62300a43ddab9a4c639
Pablo Pérez <dalotodo69@gmail.com>
2023-01-16 18:14:58 +0100

    Copy to clipboard + editor cards

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,39 @@
 <template>
   <q-page class="q-pa-md">
-    <q-toolbar class="bg-primary text-white">
-      <q-btn-dropdown stretch flat :label="dropdownLabel">
-        <q-list>
-          <template v-for="item in templates.items">
-              <q-item clickable v-close-popup tabindex="0" @click="doSelectTemplate(item)">
-                {{ t(item) }}
-              </q-item>
-          </template>
-        </q-list>
-      </q-btn-dropdown>
-    </q-toolbar>
+
     <q-splitter v-model="split" style="height: 100vh;">
       <template #before>
         <div class="q-pa-sm">
-          <CodeEditor v-model="sourceCode" class="full-width" :config="config" ></CodeEditor>
+          <q-card>
+            <q-toolbar class="bg-primary text-white">
+              <q-btn-dropdown stretch flat :label="dropdownLabel">
+                <q-list>
+                  <template v-for="item in templates.items">
+                      <q-item clickable v-close-popup tabindex="0" @click="doSelectTemplate(item)">
+                        {{ t(item) }}
+                      </q-item>
+                  </template>
+                </q-list>
+              </q-btn-dropdown>
+            </q-toolbar>
+            <q-card-section>
+              <CodeEditor v-model="sourceCode" class="full-width" :config="editorConfig" ></CodeEditor>
+            </q-card-section>
+          </q-card>
         </div>
       </template>
 
       <template #after>
         <div class="q-pa-sm">
-          <CodeEditor v-model="targetCode" class="full-width" :config="config" :readonly="true"></CodeEditor>
+          <q-card>
+            <q-toolbar class="bg-primary text-white">
+              <q-space />
+              <q-btn icon="content_copy" flat dense @click="copyToClipboard()" />
+            </q-toolbar>
+            <q-card-section>
+              <CodeEditor v-model="targetCode" class="full-width" :config="editorConfig" :readonly="true"></CodeEditor>
+            </q-card-section>
+          </q-card>
         </div>
       </template>
 
@@ -33,7 +46,10 @@
 <i18n>
   'en-US':
     'combo': 'Choose your template'
+    'copied-to-clipboard': 'Copied to clipboard!'
     'form': 'Form'
+
+
 </i18n>
 <script setup lang="ts">
 import { computed, reactive, ref } from 'vue'
@@ -45,8 +61,7 @@ import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n()
 
-
-const config = reactive({
+const editorConfig = reactive({
         disabled: false,
         indentWithTab: true,
         tabSize: 2,
@@ -59,7 +74,6 @@ const config = reactive({
 const $q = useQuasar();
 
 const split = ref(40);
-
 
 const sourceCode = ref('')
 const targetCode = ref('')
@@ -82,7 +96,6 @@ async function  doSelectTemplate(name: string) {
 async function doRender() {
   try {
     const data = JSON.parse(sourceCode.value);
-
     const code = ( await template.value?.render(data) )|| '';
     targetCode.value = code;
   } catch (err) {
@@ -93,5 +106,16 @@ async function doRender() {
   }
 }
 
+async function copyToClipboard() {
+  try {
+    navigator.clipboard.writeText(targetCode.value)
+    $q.notify( { position: 'top', type: 'positive', message: t('copied-to-clipboard'), timeout: 1000 })
+  } catch (err) {
+    $q.notify({
+      type: 'negative',
+      message: `${err}`
+    })
+  }
+}
 
 </script>
